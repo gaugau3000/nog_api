f5ff4ca2b84c22ac9983e16ab813afee2b3bd52e
gaugau3000 <g.pialat@gmail.com>
2020-04-21 15:37:04 +0000

    fix deploy ci script

--- scripts/ci/deploy_container.sh
+++ scripts/ci/deploy_container.sh
@@ -6,4 +6,4 @@ docker run -d --restart unless-stopped --name "${CONTENER_NAME}" \
 --env MAIl_HOST=notonlygeek_mail --env SMTP_PORT=25 \
 --network notonlygeek \
 "${GITHUB_REPOSITORY}":latest
-docker network connect "${CONTENER_NAME}" notonlygeek_"${APP_ENV}" --alias api
+docker network connect "notonlygeek_${APP_ENV}" "${CONTENER_NAME}" --alias api
