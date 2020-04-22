aa744a270ac9fcc3b444b14cd0196556dcc2b20f
gaugau3000 <g.pialat@gmail.com>
2020-04-22 13:46:23 +0000

    remove unused files

--- scripts/ci/publish_images.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker push gaugau3000/nog_api:install_dev
-docker push gaugau3000/nog_api:install_prod
-docker push gaugau3000/nog_api:test
-docker push gaugau3000/nog_api:build
-docker push gaugau3000/nog_api:latest
--- scripts/ci/pull_from_cache.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-docker pull gaugau3000/nog_api:install_dev || true
-docker pull gaugau3000/nog_api:install_prod || true
-docker pull gaugau3000/nog_api:test || true
-docker pull gaugau3000/nog_api:build || true
-docker pull gaugau3000/nog_api:latest || true
