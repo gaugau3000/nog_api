109906085f55c3360a628cf9c13006cfd0de465f
gaugau3000 <g.pialat@gmail.com>
2020-04-22 08:48:33 +0000

    ignore tls because use local relay

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,4 +5,6 @@ import SMTPTransport from 'nodemailer/lib/smtp-transport'
 export const stmp_config:SMTPTransport.Options = {
     host: process.env.MAIl_HOST,
     port: Number(process.env.SMTP_PORT),
+    secure: false,
+    ignoreTLS: true
   }
