c7398872e600ac4b7d9c510398ddb04efee88d21
gaugau3000 <g.pialat@gmail.com>
2020-05-12 12:27:27 +0000

    prospect contact form mail

--- src/mail_templates/mailHeaders.ts
+++ src/mail_templates/mailHeaders.ts
@@ -3,5 +3,9 @@ export default {
         from: "contact@notonlygeek.com",
         to: "contact@notonlygeek.com",
         subject: "Mail envoyé depuis le formulaire de contact"
+    },
+    contact_form_prospect: {
+        from: "contact@notonlygeek.com",
+        subject: "Merci de trouver les coordonnées de Notonlygeek !"
     }
 }
