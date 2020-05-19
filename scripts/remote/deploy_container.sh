3e63a3ce637645c4c410c5ff64db236f1cbd6fd5
gaugau3000 <g.pialat@gmail.com>
2020-05-19 12:45:31 +0000

    ci for production

--- scripts/remote/deploy_container.sh
+++ scripts/remote/deploy_container.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+docker_image="${GITHUB_REPOSITORY}":"${GITHUB_HEAD_REF}"
 docker stop "${CONTENER_NAME}" || true
 docker rm "${CONTENER_NAME}" || true
-docker pull "${GITHUB_REPOSITORY}"
+docker pull "${docker_image}"
 docker run -d --restart unless-stopped --name "${CONTENER_NAME}" \
 --env MAIl_HOST=notonlygeek_mail --env SMTP_PORT=25 \
 --network notonlygeek \
-"${GITHUB_REPOSITORY}":latest
+"${docker_image}"
 docker network connect "${NETWORK}" "${CONTENER_NAME}" --alias api
 docker run --rm --network "${NETWORK}" willwill/wait-for-it api:3000
