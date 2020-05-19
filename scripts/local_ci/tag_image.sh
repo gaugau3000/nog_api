5dd5f1d4a79b70ed03e321db26b6e8063f616565
gaugau3000 <g.pialat@gmail.com>
2020-05-19 15:01:57 +0000

    fix ci

--- scripts/local_ci/tag_image.sh
+++ scripts/local_ci/tag_image.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+docker pull gaugau3000/nog_api:tmp
 docker login gaugau3000 -p "${DOCKER_PASSWORD}"
 docker tag gaugau3000/nog_api:tmp gaugau3000/nog_api:"${GITHUB_REF_SLUG}"
 docker push gaugau3000/nog_api:"${GITHUB_REF_SLUG}"
