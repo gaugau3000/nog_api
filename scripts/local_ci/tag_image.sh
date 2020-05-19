ab4fd1b9a1195bf27c1ba484b702dfe8403b8f64
gaugau3000 <g.pialat@gmail.com>
2020-05-19 16:19:36 +0000

    fix ci

--- scripts/local_ci/tag_image.sh
+++ scripts/local_ci/tag_image.sh
@@ -6,6 +6,6 @@ docker push gaugau3000/nog_api:"${GITHUB_REF_SLUG}"
 
 if [ "$GITHUB_REF_SLUG" = "master" ]
 then
-    docker tag gaugau3000/nog_api:tmp gaugau3000/nog_api:latest \
+    docker tag gaugau3000/nog_api:tmp gaugau3000/nog_api:latest
     docker push gaugau3000/nog_api:latest
 fi
