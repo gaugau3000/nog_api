78d631109cc659833a0c609240b5576026c3d90c
gaugau3000 <g.pialat@gmail.com>
2020-05-19 14:37:15 +0000

    fix ci

--- scripts/local_ci/tag_image.sh
+++ scripts/local_ci/tag_image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker login gaugau3000 -p "${DOCKER_PASSWORD}"
+docker login gaugau3000 --password-stdin "${DOCKER_PASSWORD}"
 docker tag gaugau3000/nog_api:tmp gaugau3000/nog_api:"${GIT_BRANCH}"
 docker push gaugau3000/nog_api:"${GIT_BRANCH}"
 
